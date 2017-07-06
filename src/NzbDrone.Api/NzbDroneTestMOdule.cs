a881211dd03d846c37352a374e89767b697a0095
Taloth Saldono <Taloth@users.noreply.github.com>
2017-07-06 21:00:42 +0200

    Added simple test api and disabled apikey.

---
+++ src/NzbDrone.Api/NzbDroneTestMOdule.cs
@@ -0,0 +1,31 @@
+﻿using Nancy;
+using Newtonsoft.Json.Linq;
+using NzbDrone.Common.Http;
+
+namespace NzbDrone.Api
+{
+    public  class NzbDroneTestModule : NancyModule
+    {
+        private readonly IHttpClient _httpClient;
+
+        protected NzbDroneTestModule(IHttpClient httpClient)
+            : base("/test")
+        {
+            _httpClient = httpClient;
+
+            Get["/"] = _ => DoTest();
+        }
+
+        private Response DoTest()
+        {
+            var requestBuilder = new HttpRequestBuilder("https://torrentapi.org")
+                           .WithRateLimit(3.0)
+                           .Resource("/pubapi_v2.php?get_token=get_token&app_id=Sonarr")
+                           .Accept(HttpAccept.Json);
+
+            var response = _httpClient.Get<JObject>(requestBuilder.Build());
+
+            return Response.AsText(response.ToString());
+        }
+    }
+}
